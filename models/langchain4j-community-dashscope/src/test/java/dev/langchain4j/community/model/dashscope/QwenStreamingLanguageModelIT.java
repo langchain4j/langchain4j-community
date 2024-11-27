3cd0169255a2192ee9db26f9d6f5730651176c94
Martin7-1 <yi.zheng.se@gmail.com>
2024-11-27 16:57:32 +0800

    migrate dashscope

---
+++ models/langchain4j-community-dashscope/src/test/java/dev/langchain4j/community/model/dashscope/QwenStreamingLanguageModelIT.java
@@ -0,0 +1,29 @@
+package dev.langchain4j.community.model.dashscope;
+
+import dev.langchain4j.model.chat.TestStreamingResponseHandler;
+import dev.langchain4j.model.language.StreamingLanguageModel;
+import dev.langchain4j.model.output.Response;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static dev.langchain4j.community.model.dashscope.QwenTestHelper.apiKey;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@EnabledIfEnvironmentVariable(named = "DASHSCOPE_API_KEY", matches = ".+")
+class QwenStreamingLanguageModelIT {
+
+    @ParameterizedTest
+    @MethodSource("dev.langchain4j.community.model.dashscope.QwenTestHelper#languageModelNameProvider")
+    void should_send_messages_and_receive_response(String modelName) {
+        StreamingLanguageModel model = QwenStreamingLanguageModel.builder()
+                .apiKey(apiKey())
+                .modelName(modelName)
+                .build();
+        TestStreamingResponseHandler<String> handler = new TestStreamingResponseHandler<>();
+        model.generate("Please say 'hello' to me", handler);
+        Response<String> response = handler.get();
+
+        assertThat(response.content()).containsIgnoringCase("hello");
+    }
+}
