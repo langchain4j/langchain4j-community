0f4bd3d3f6f7ee99ef162804ec3933c9f193b8b4
Sualeh Fatehi <sualeh@hotmail.com>
2025-01-31 18:59:05 -0500

    Add javadoc

--- content-retrievers/langchain4j-community-lucene/src/main/java/dev/langchain4j/rag/content/retriever/lucene/LuceneFields.java
+++ content-retrievers/langchain4j-community-lucene/src/main/java/dev/langchain4j/rag/content/retriever/lucene/LuceneFields.java
@@ -1,9 +1,14 @@
 package dev.langchain4j.rag.content.retriever.lucene;
 
+/** Default field names used when storing data in Lucene. Used for both storage and retrieval. **/
 public enum LuceneFields {
+    /** Id of the Lucene document **/
     ID_FIELD_NAME("id"),
+    /** Text content of the content **/
     CONTENT_FIELD_NAME("content"),
+    /** Estimated token count of the content **/
     TOKEN_COUNT_FIELD_NAME("estimated-token-count"),
+    /** Embedding vector of the content **/
     EMBEDDING_FIELD_NAME("embedding");
 
     private final String fieldName;
@@ -12,6 +17,11 @@ public enum LuceneFields {
         this.fieldName = fieldName;
     }
 
+    /**
+     * Field name used in the Lucene document.
+     *
+     * @return Field name
+     */
     public String fieldName() {
         return fieldName;
     }
