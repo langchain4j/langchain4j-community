2dad9a57d323ebcdd5d7beccdd10182a33b6b780
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2025-03-07 15:31:59 +0800

    Add ITs for new parameters

--- models/langchain4j-community-dashscope/src/test/java/dev/langchain4j/community/model/dashscope/WanxImageModelIT.java
+++ models/langchain4j-community-dashscope/src/test/java/dev/langchain4j/community/model/dashscope/WanxImageModelIT.java
@@ -1,17 +1,18 @@
 package dev.langchain4j.community.model.dashscope;
 
-import static dev.langchain4j.community.model.dashscope.QwenTestHelper.apiKey;
-import static dev.langchain4j.community.model.dashscope.QwenTestHelper.multimodalImageData;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import dev.langchain4j.data.image.Image;
 import dev.langchain4j.model.output.Response;
-import java.net.URI;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+
+import static dev.langchain4j.community.model.dashscope.QwenTestHelper.apiKey;
+import static dev.langchain4j.community.model.dashscope.QwenTestHelper.multimodalImageData;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @EnabledIfEnvironmentVariable(named = "DASHSCOPE_API_KEY", matches = ".+")
 class WanxImageModelIT {
@@ -25,6 +26,48 @@ class WanxImageModelIT {
                 WanxImageModel.builder().apiKey(apiKey()).modelName(modelName).build();
 
         Response<Image> response = model.generate("Beautiful house on country side");
+
+        URI remoteImage = response.content().url();
+        log.info("Your remote image is here: {}", remoteImage);
+        assertThat(remoteImage).isNotNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource("dev.langchain4j.community.model.dashscope.WanxTestHelper#imageModelNameProvider")
+    void simple_image_generation_works_with_negative_prompt(String modelName) {
+        WanxImageModel model =
+                WanxImageModel.builder().apiKey(apiKey()).modelName(modelName).negativePrompt("dog").build();
+
+        Response<Image> response = model.generate("Cat and dog");
+
+        URI remoteImage = response.content().url();
+        log.info("Your remote image is here: {}", remoteImage);
+        assertThat(remoteImage).isNotNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource("dev.langchain4j.community.model.dashscope.WanxTestHelper#imageModelNameV2Provider")
+    void simple_image_generation_works_with_prompt_extend(String modelName) {
+        WanxImageModel model =
+                WanxImageModel.builder().apiKey(apiKey()).modelName(modelName).promptExtend(true).build();
+
+        Response<Image> response = model.generate("Cat and dog");
+
+        URI remoteImage = response.content().url();
+        String revisedPrompt = response.content().revisedPrompt();
+        log.info("Your remote image is here: {}", remoteImage);
+        log.info("Your revised prompt is: {}", revisedPrompt);
+        assertThat(remoteImage).isNotNull();
+        assertThat(revisedPrompt).isNotBlank();
+    }
+
+    @ParameterizedTest
+    @MethodSource("dev.langchain4j.community.model.dashscope.WanxTestHelper#imageModelNameV2Provider")
+    void simple_image_generation_works_with_watermark(String modelName) {
+        WanxImageModel model =
+                WanxImageModel.builder().apiKey(apiKey()).modelName(modelName).watermark(true).build();
+
+        Response<Image> response = model.generate("Cat and dog");
 
         URI remoteImage = response.content().url();
         log.info("Your remote image is here: {}", remoteImage);
--- models/langchain4j-community-dashscope/src/test/java/dev/langchain4j/community/model/dashscope/WanxTestHelper.java
+++ models/langchain4j-community-dashscope/src/test/java/dev/langchain4j/community/model/dashscope/WanxTestHelper.java
@@ -8,7 +8,20 @@ class WanxTestHelper {
 
     public static Stream<Arguments> imageModelNameProvider() {
         return Stream.of(
-                Arguments.of(WanxModelName.WANX_V1)
+                Arguments.of(WanxModelName.WANX_V1),
+                Arguments.of(WanxModelName.WANX2_0_T2I_TURBO),
+                Arguments.of(WanxModelName.WANX2_1_T2I_TURBO),
+                Arguments.of(WanxModelName.WANX2_1_T2I_PLUS)
+        );
+    }
+
+    // Some parameter features are only supported by v2
+    // (v1 will not throw exceptions, but have no effect)
+    public static Stream<Arguments> imageModelNameV2Provider() {
+        return Stream.of(
+                Arguments.of(WanxModelName.WANX2_0_T2I_TURBO),
+                Arguments.of(WanxModelName.WANX2_1_T2I_TURBO),
+                Arguments.of(WanxModelName.WANX2_1_T2I_PLUS)
         );
     }
 }
