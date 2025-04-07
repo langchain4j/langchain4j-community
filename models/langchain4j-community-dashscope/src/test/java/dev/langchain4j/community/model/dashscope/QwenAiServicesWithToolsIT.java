3ac6646f34cbfb0d36d0a94deacc9cfd3daf404f
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2025-04-07 16:31:39 +0800

    fix: revert QwenAiServicesWithToolsIT.java

--- models/langchain4j-community-dashscope/src/test/java/dev/langchain4j/community/model/dashscope/QwenAiServicesWithToolsIT.java
+++ models/langchain4j-community-dashscope/src/test/java/dev/langchain4j/community/model/dashscope/QwenAiServicesWithToolsIT.java
@@ -5,12 +5,12 @@ import static dev.langchain4j.community.model.dashscope.QwenTestHelper.apiKey;
 import static java.util.Collections.singletonList;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.service.common.AbstractAiServiceIT;
+import dev.langchain4j.service.common.AbstractAiServiceWithToolsIT;
 import java.util.List;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 @EnabledIfEnvironmentVariable(named = "DASHSCOPE_API_KEY", matches = ".+")
-class QwenAiServicesWithToolsIT extends AbstractAiServiceIT {
+class QwenAiServicesWithToolsIT extends AbstractAiServiceWithToolsIT {
 
     @Override
     protected List<ChatLanguageModel> models() {
