73d05ac34db73596c87bbf16ce6352748ba48892
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2025-04-11 12:02:27 +0800

    chore: make format

--- models/langchain4j-community-dashscope/src/test/java/dev/langchain4j/community/model/dashscope/QwenStreamingAiServiceIT.java
+++ models/langchain4j-community-dashscope/src/test/java/dev/langchain4j/community/model/dashscope/QwenStreamingAiServiceIT.java
@@ -1,14 +1,13 @@
 package dev.langchain4j.community.model.dashscope;
 
-import dev.langchain4j.model.chat.StreamingChatLanguageModel;
-import dev.langchain4j.service.common.AbstractStreamingAiServiceIT;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
-
-import java.util.List;
-
 import static dev.langchain4j.community.model.dashscope.QwenModelName.QWEN_MAX;
 import static dev.langchain4j.community.model.dashscope.QwenTestHelper.apiKey;
 import static java.util.Collections.singletonList;
+
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.service.common.AbstractStreamingAiServiceIT;
+import java.util.List;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 @EnabledIfEnvironmentVariable(named = "DASHSCOPE_API_KEY", matches = ".+")
 public class QwenStreamingAiServiceIT extends AbstractStreamingAiServiceIT {
