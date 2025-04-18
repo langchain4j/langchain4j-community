e1a595b42b3d0ff955de88a47e0275ff6afcb0e0
vga91 <giuseppe.villani@larus-ba.it>
2025-04-18 15:18:42 +0200

    changes review

--- content-retrievers/langchain4j-community-neo4j-retriever/src/main/java/dev/langchain4j/community/rag/content/retriever/neo4j/Neo4jGraph.java
+++ content-retrievers/langchain4j-community-neo4j-retriever/src/main/java/dev/langchain4j/community/rag/content/retriever/neo4j/Neo4jGraph.java
@@ -1,5 +1,6 @@
 package dev.langchain4j.community.rag.content.retriever.neo4j;
 
+import static dev.langchain4j.community.rag.content.retriever.neo4j.Neo4jGraphSchemaUtils.getSchemaFromMetadata;
 import static dev.langchain4j.internal.Utils.getOrDefault;
 
 import dev.langchain4j.internal.ValidationUtils;
@@ -42,25 +43,10 @@ public class Neo4jGraph implements AutoCloseable {
             return this;
         }
 
-        Neo4jGraph build() {
+        public Neo4jGraph build() {
             return new Neo4jGraph(driver, sample, maxRels);
         }
     }
-
-    private static final String SCHEMA_FROM_META_DATA =
-            """
-            CALL apoc.meta.data({maxRels: $maxRels, sample: $sample})
-            YIELD label, other, elementType, type, property
-            WITH label, elementType,
-                 apoc.text.join(collect(case when NOT type = "RELATIONSHIP" then property+": "+type else null end),", ") AS properties,
-                 collect(case when type = "RELATIONSHIP" AND elementType = "node" then "(:" + label + ")-[:" + property + "]->(:" + toString(other[0]) + ")" else null end) AS patterns
-            WITH elementType AS type,
-                apoc.text.join(collect(":"+label+" {"+properties+"}"),"\\n") AS entities,
-                apoc.text.join(apoc.coll.flatten(collect(coalesce(patterns,[]))),"\\n") AS patterns
-            RETURN collect(case type when "relationship" then entities end)[0] AS relationships,
-                collect(case type when "node" then entities end)[0] AS nodes,
-                collect(case type when "node" then patterns end)[0] as patterns
-            """;
 
     private final Driver driver;
     private final Long sample;
@@ -115,16 +101,7 @@ public class Neo4jGraph implements AutoCloseable {
     }
 
     public void refreshSchema() {
-        final Record record = executeRead(SCHEMA_FROM_META_DATA, Map.of("sample", sample, "maxRels", maxRels))
-                .get(0);
-        final String nodesString = record.get("nodes").asString();
-        final String relationshipsString = record.get("relationships").asString();
-        final String patternsString = record.get("patterns").asString();
-        this.schema = "Node properties are the following:\n" + nodesString
-                + "\n\n" + "Relationship properties are the following:\n"
-                + relationshipsString
-                + "\n\n" + "The relationships are the following:\n"
-                + patternsString;
+        this.schema = getSchemaFromMetadata(this, sample, maxRels);
     }
 
     @Override
