2e9b72abbcd3f5de5d0715ea97402585085b6b04
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2025-04-24 18:12:09 +0200

    Neo4jGraph features (#119)
    * Neo4j Integration Improvements List: Combine metadata retrieval into one query, Add option for enhanced schema with sample property values
    
    * Integration test for invalid query for Neo4jGraph
    
    * make format
    
    * changes review
    
    * changed builder methods to public

--- content-retrievers/langchain4j-community-neo4j-retriever/src/main/java/dev/langchain4j/community/rag/content/retriever/neo4j/Neo4jGraph.java
+++ content-retrievers/langchain4j-community-neo4j-retriever/src/main/java/dev/langchain4j/community/rag/content/retriever/neo4j/Neo4jGraph.java
@@ -1,13 +1,14 @@
 package dev.langchain4j.community.rag.content.retriever.neo4j;
 
+import static dev.langchain4j.community.rag.content.retriever.neo4j.Neo4jGraphSchemaUtils.getSchemaFromMetadata;
+import static dev.langchain4j.internal.Utils.getOrDefault;
+
 import dev.langchain4j.internal.ValidationUtils;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Record;
 import org.neo4j.driver.Session;
-import org.neo4j.driver.Value;
 import org.neo4j.driver.exceptions.ClientException;
 import org.neo4j.driver.summary.ResultSummary;
 
@@ -15,53 +16,48 @@ public class Neo4jGraph implements AutoCloseable {
 
     public static class Builder {
         private Driver driver;
+        private Long sample;
+        private Long maxRels;
 
         /**
          * @param driver the {@link Driver} (required)
          */
-        Builder driver(Driver driver) {
+        public Builder driver(Driver driver) {
             this.driver = driver;
             return this;
         }
 
-        Neo4jGraph build() {
-            return new Neo4jGraph(driver);
+        /**
+         * @param sample number of nodes to sample per label with the `apoc.meta.data` procedure (default: 1000)
+         */
+        public Builder sample(Long sample) {
+            this.sample = sample;
+            return this;
+        }
+
+        /**
+         * @param maxRels the maximum number of relationships to look at per Node Label with the `apoc.meta.data` procedure (default: 100)
+         */
+        public Builder maxRels(Long maxRels) {
+            this.maxRels = maxRels;
+            return this;
+        }
+
+        public Neo4jGraph build() {
+            return new Neo4jGraph(driver, sample, maxRels);
         }
     }
 
-    private static final String NODE_PROPERTIES_QUERY =
-            """
-                    CALL apoc.meta.data()
-                    YIELD label, other, elementType, type, property
-                    WHERE NOT type = "RELATIONSHIP" AND elementType = "node"
-                    WITH label AS nodeLabels, collect({property:property, type:type}) AS properties
-                    RETURN {labels: nodeLabels, properties: properties} AS output
-                    """;
-
-    private static final String REL_PROPERTIES_QUERY =
-            """
-                    CALL apoc.meta.data()
-                    YIELD label, other, elementType, type, property
-                    WHERE NOT type = "RELATIONSHIP" AND elementType = "relationship"
-                    WITH label AS nodeLabels, collect({property:property, type:type}) AS properties
-                    RETURN {type: nodeLabels, properties: properties} AS output
-                    """;
-
-    private static final String RELATIONSHIPS_QUERY =
-            """
-                    CALL apoc.meta.data()
-                    YIELD label, other, elementType, type, property
-                    WHERE type = "RELATIONSHIP" AND elementType = "node"
-                    UNWIND other AS other_node
-                    RETURN {start: label, type: property, end: toString(other_node)} AS output
-                    """;
-
     private final Driver driver;
+    private final Long sample;
+    private final Long maxRels;
 
     private String schema;
 
-    public Neo4jGraph(final Driver driver) {
+    public Neo4jGraph(final Driver driver, Long sample, Long maxRels) {
 
+        this.sample = getOrDefault(sample, 1000L);
+        this.maxRels = getOrDefault(maxRels, 100L);
         this.driver = ValidationUtils.ensureNotNull(driver, "driver");
         this.driver.verifyConnectivity();
         try {
@@ -78,7 +74,7 @@ public class Neo4jGraph implements AutoCloseable {
         return schema;
     }
 
-    static Builder builder() {
+    public static Builder builder() {
         return new Builder();
     }
 
@@ -92,60 +88,20 @@ public class Neo4jGraph implements AutoCloseable {
     }
 
     public List<Record> executeRead(String queryString) {
+        return executeRead(queryString, Map.of());
+    }
 
-        return this.driver.executableQuery(queryString).execute().records();
+    public List<Record> executeRead(String queryString, Map<String, Object> parameters) {
+
+        return this.driver
+                .executableQuery(queryString)
+                .withParameters(parameters)
+                .execute()
+                .records();
     }
 
     public void refreshSchema() {
-
-        List<String> nodeProperties = formatNodeProperties(executeRead(NODE_PROPERTIES_QUERY));
-        List<String> relationshipProperties = formatRelationshipProperties(executeRead(REL_PROPERTIES_QUERY));
-        List<String> relationships = formatRelationships(executeRead(RELATIONSHIPS_QUERY));
-
-        this.schema = "Node properties are the following:\n" + String.join("\n", nodeProperties)
-                + "\n\n" + "Relationship properties are the following:\n"
-                + String.join("\n", relationshipProperties)
-                + "\n\n" + "The relationships are the following:\n"
-                + String.join("\n", relationships);
-    }
-
-    private List<String> formatNodeProperties(List<Record> records) {
-
-        return records.stream()
-                .map(this::getOutput)
-                .map(r -> String.format(
-                        "%s %s",
-                        r.asMap().get("labels"), formatMap(r.get("properties").asList(Value::asMap))))
-                .toList();
-    }
-
-    private List<String> formatRelationshipProperties(List<Record> records) {
-
-        return records.stream()
-                .map(this::getOutput)
-                .map(r -> String.format(
-                        "%s %s", r.get("type"), formatMap(r.get("properties").asList(Value::asMap))))
-                .toList();
-    }
-
-    private List<String> formatRelationships(List<Record> records) {
-
-        return records.stream()
-                .map(r -> getOutput(r).asMap())
-                .map(r -> String.format("(:%s)-[:%s]->(:%s)", r.get("start"), r.get("type"), r.get("end")))
-                .toList();
-    }
-
-    private Value getOutput(Record record) {
-
-        return record.get("output");
-    }
-
-    private String formatMap(List<Map<String, Object>> properties) {
-
-        return properties.stream()
-                .map(prop -> prop.get("property") + ":" + prop.get("type"))
-                .collect(Collectors.joining(", ", "{", "}"));
+        this.schema = getSchemaFromMetadata(this, sample, maxRels);
     }
 
     @Override
