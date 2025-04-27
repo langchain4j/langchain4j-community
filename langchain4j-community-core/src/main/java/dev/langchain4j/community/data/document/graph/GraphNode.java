ba99ea0ac03dfb622224121413c8550f5344d69a
Martin7-1 <yi.zheng.se@gmail.com>
2025-04-27 16:41:29 +0800

    Introduce new `GraphTransformer` interface (#153)
    * New GraphTransformer interface
    
    * Add tests for GraphTransformer
    
    * add bom
    
    * Add @Experimental

---
+++ langchain4j-community-core/src/main/java/dev/langchain4j/community/data/document/graph/GraphNode.java
@@ -0,0 +1,67 @@
+package dev.langchain4j.community.data.document.graph;
+
+import static dev.langchain4j.internal.Utils.copyIfNotNull;
+import static dev.langchain4j.internal.Utils.getOrDefault;
+
+import dev.langchain4j.Experimental;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Represents a node in a graph with associated properties.
+ *
+ * @since 1.0.0-beta4
+ */
+@Experimental
+public class GraphNode {
+
+    private final String id;
+    private final String type;
+    private final Map<String, String> properties;
+
+    public GraphNode(String id, String type, Map<String, String> properties) {
+        this.id = id;
+        this.type = getOrDefault(type, "Node");
+        this.properties = copyIfNotNull(properties);
+    }
+
+    public String id() {
+        return id;
+    }
+
+    public String type() {
+        return type;
+    }
+
+    public Map<String, String> properties() {
+        return properties;
+    }
+
+    public static GraphNode from(String id, String type, Map<String, String> properties) {
+        return new GraphNode(id, type, properties);
+    }
+
+    public static GraphNode from(String id) {
+        return new GraphNode(id, null, new HashMap<>());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GraphNode graphNode = (GraphNode) o;
+        return Objects.equals(id, graphNode.id)
+                && Objects.equals(type, graphNode.type)
+                && Objects.equals(properties, graphNode.properties);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, type, properties);
+    }
+
+    @Override
+    public String toString() {
+        return "GraphNode{" + "id='" + id + '\'' + ", type='" + type + '\'' + ", properties=" + properties + '}';
+    }
+}
