604e3b18367a6aee94b196045730e2924e483aea
Harikrishna <harikrishna553@gmail.com>
2025-08-16 14:19:40 +0530

    Add HybridEmbeddingStore: In-Memory Embeddings with Disk-Based Chunk (#326)
    * Add HybridEmbeddingStore: In-Memory Embeddings with Disk-Based Chunk
    
    * Fix Review Comments
    
    * Rename Embedding Store from Hybrid to MemFileEmbeddingStore
    
    * Update version to 1.4.0-beta10-SNAPSHOT
    
    * Use Strategy Pattern to serialize and deserialize EmbeddedStore Metadata
    
    * Remove redundant @JsonProperty annotation
    
    * Follow Naming convention for test class
    
    * Address Review Comments
    
    * Add default methods for searialization and deserialization
    
    * Make the cache synchronized
    
    * docs: fix javadocs
    
    ---------
    
    Co-authored-by: Harikrishna <harikrishna.gurram@walmart.com>
    Co-authored-by: Martin7-1 <yi.zheng.se@gmail.com>

---
+++ embedding-stores/langchain4j-community-memfile/src/test/java/dev/langchain4j/community/store/embedding/memfile/MemFileEmbeddingStoreIT.java
@@ -0,0 +1,39 @@
+package dev.langchain4j.community.store.embedding.memfile;
+
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2QuantizedEmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreWithFilteringIT;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+public class MemFileEmbeddingStoreIT extends EmbeddingStoreWithFilteringIT {
+
+    private EmbeddingModel embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+    private MemFileEmbeddingStore<TextSegment> embeddingStore = new MemFileEmbeddingStore<>(createTempDirectory());
+
+    public static Path createTempDirectory() {
+        try {
+            return Files.createTempDirectory(UUID.randomUUID().toString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected EmbeddingStore<TextSegment> embeddingStore() {
+        return embeddingStore;
+    }
+
+    @Override
+    protected EmbeddingModel embeddingModel() {
+        return embeddingModel;
+    }
+
+    @Override
+    protected boolean supportsContains() {
+        return true;
+    }
+}
