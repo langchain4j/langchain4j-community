8fdbc336c943b3f82b32bc56cb02fc41c6e00293
odysseaspenta <odysseas@sysnetint.com>
2025-11-19 03:59:55 -0500

    Add support for jvector as an embedding store (#459)
    * Added support for jvector as an embedding store
    
    * Synced the version number with that of the rest of the tree
    
    * Made a change to ensure that the index is persisted if the user has requested that option.
    
    * Fixed the formatting issues that were causing spotless verify to fail.
    
    * Added a parameter that controls how often the index is rebuilt, allowing the user to use an eager approach by setting the parameter to 1, a lazy approach by setting the parameter to 0 and some larger value to rebuild it periodically.
    
    * rewrite test with openrewrite
    
    ---------
    
    Co-authored-by: Martin7-1 <yi.zheng.se@gmail.com>

---
+++ embedding-stores/langchain4j-community-jvector/src/test/java/dev/langchain4j/community/store/embedding/jvector/JVectorEmbeddingStoreRemovalIT.java
@@ -0,0 +1,36 @@
+package dev.langchain4j.community.store.embedding.jvector;
+
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2QuantizedEmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreWithRemovalIT;
+import org.junit.jupiter.api.AfterEach;
+
+class JVectorEmbeddingStoreRemovalIT extends EmbeddingStoreWithRemovalIT {
+
+    EmbeddingModel embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+
+    EmbeddingStore<TextSegment> embeddingStore =
+            JVectorEmbeddingStore.builder().dimension(384).build();
+
+    @AfterEach
+    void afterEach() {
+        embeddingStore.removeAll();
+    }
+
+    @Override
+    protected boolean supportsRemoveAllByFilter() {
+        return false;
+    }
+
+    @Override
+    protected EmbeddingStore<TextSegment> embeddingStore() {
+        return embeddingStore;
+    }
+
+    @Override
+    protected EmbeddingModel embeddingModel() {
+        return embeddingModel;
+    }
+}
