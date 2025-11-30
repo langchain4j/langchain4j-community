ebbb7f6debe5dddfb768d4976356c1fc036b377f
Xin Wang <best.wangxin@163.com>
2025-11-30 11:51:20 +0000

    Browser Use Support

---
+++ browser-use/langchain4j-community-browser-use/src/test/java/dev/langchain4j/community/browser/use/BrowserUseToolIT.java
@@ -0,0 +1,45 @@
+package dev.langchain4j.community.browser.use;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.service.AiServices;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+
+@EnabledIfEnvironmentVariable(named = "OPENAI_API_KEY", matches = ".+")
+public class BrowserUseToolIT {
+
+    OpenAiChatModel model = OpenAiChatModel.builder()
+            .baseUrl(System.getenv("OPENAI_BASE_URL"))
+            .apiKey(System.getenv("OPENAI_API_KEY"))
+            .organizationId(System.getenv("OPENAI_ORGANIZATION_ID"))
+            .modelName(GPT_4_O_MINI)
+            .logRequests(true)
+            .logResponses(true)
+            .build();
+
+    interface Assistant {
+
+        String chat(String userMessage);
+    }
+
+    @Test
+    void should_execute_tool() {
+
+        BrowserUseTool tool = new BrowserUseTool();
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatModel(model)
+                .tools(tool)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                .build();
+
+        String answer = assistant.chat("open page 'https://docs.langchain4j.dev/', and summary the page text");
+
+        assertThat(answer).contains("Java");
+        assertThat(answer).contains("LLM");
+    }
+}
