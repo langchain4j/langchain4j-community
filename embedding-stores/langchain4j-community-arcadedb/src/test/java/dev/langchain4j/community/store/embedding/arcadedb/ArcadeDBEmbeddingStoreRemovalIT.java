2fb2ba4626597537519af25efa4771c5de8f8de2
odysseaspenta <odysseas@sysnetint.com>
2026-03-03 16:58:41 -0500

    Minor fix to the removal test cases to ensure that they run successfully.

--- embedding-stores/langchain4j-community-arcadedb/src/test/java/dev/langchain4j/community/store/embedding/arcadedb/ArcadeDBEmbeddingStoreRemovalIT.java
+++ embedding-stores/langchain4j-community-arcadedb/src/test/java/dev/langchain4j/community/store/embedding/arcadedb/ArcadeDBEmbeddingStoreRemovalIT.java
@@ -1,14 +1,25 @@
 package dev.langchain4j.community.store.embedding.arcadedb;
 
+import dev.langchain4j.data.document.Metadata;
+import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2QuantizedEmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreWithRemovalIT;
+import dev.langchain4j.store.embedding.TestUtils;
+import dev.langchain4j.store.embedding.filter.MetadataFilterBuilder;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
 import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.util.List;
 
 public class ArcadeDBEmbeddingStoreRemovalIT extends EmbeddingStoreWithRemovalIT {
 
@@ -49,6 +60,11 @@ public class ArcadeDBEmbeddingStoreRemovalIT extends EmbeddingStoreWithRemovalIT
     }
 
     EmbeddingModel embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+
+    @BeforeEach
+    void clearStore() {
+        embeddingStore().removeAll();
+    }
 
     @Override
     protected EmbeddingStore<TextSegment> embeddingStore() {
